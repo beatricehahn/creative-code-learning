d7c04d8a3e56e0990b6b8dc759813638269ec47d
Beatrice (Con) Hahn <hahnb@usc.edu>
2022-06-04 04:14:00 -0700

    Render cube using local server and webpack

--- threejs-journey/04-webpack/src/script.js
+++ threejs-journey/04-webpack/src/script.js
@@ -1,1 +1,38 @@
-import './style.css'
+import './style.css';
+import * as THREE from 'three';
+
+// Create a scene
+const scene = new THREE.Scene();
+
+// Red cube created by using mesh (1,1,1) are the measurement parameters
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({ color: 'red' });
+// Mesh is a combination of geometry and material
+const mesh = new THREE.Mesh(geometry, material);
+
+// Add mesh to the scene
+scene.add(mesh);
+
+// Set sizes object
+const sizes = {
+    width: 800,
+    height: 600
+};
+
+// Camera by adding 1) field of view and 2) aspect ratio
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// Change camera position
+camera.position.z = 3;
+camera.position.y = 0;
+camera.position.x = 1;
+// Add camera to scene
+scene.add(camera);
+
+// WebGL Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer ({
+    canvas: canvas
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
